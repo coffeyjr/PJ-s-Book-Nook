0b54852152090ae4908f55d8849f748ccd3e3035
coffeyjr <jacksonrcoffey@gmail.com>
2023-07-16 22:52:23 -0400

    Test 1 (Eleventy)

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('src/style.css');
+    eleventyConfig.addPassthroughCopy('src/genres.css');
+    eleventyConfig.addPassthroughCopy('src/books.css');
+    eleventyConfig.addPassthroughCopy('src/images');
+    eleventyConfig.addPassthroughCopy('src/fonts');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
